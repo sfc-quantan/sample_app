db2a048530441f159a8184272a926d028d88df7a
quantan <quantan@ht.sfc.keio.ac.jp>
2017-10-03 11:07:43 +0900

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root 'tatic_pages#home'
+
+
   get 'static_pages/home'
 
   get 'static_pages/help'
-
-    root 'application#hello'
+  
+  root 'application#hello'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
